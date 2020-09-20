dadc0f1d0dc0bc8674df02681f2086ad0ed0489f
teeradon43 <teeradonch43@gmail.com>
2020-09-20 15:36:00 +0700

    printNto1

--- Recursion/lab1-recursion.py
+++ Recursion/lab1-recursion.py
@@ -17,5 +17,12 @@ def sum1ToN(n): # return sum of 1 -> n while n >= 1
 		else:
 			return sum1ToN(n-1)+n
 
+def printNto1(n): #print number n to 1
+	if n == 1:
+		print(n)
+	else:
+		print(n)
+		return printNto1(n-1)
+
 if __name__ == '__main__':
-	print(sum1ToN(7))
+	printNto1(7)
