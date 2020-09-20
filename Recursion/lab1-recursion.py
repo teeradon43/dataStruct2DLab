78da3a967c7de5823aeeaa69ac41c3ecff9aa6f2
teeradon43 <teeradonch43@gmail.com>
2020-09-20 15:32:46 +0700

    sum1ToN

--- Recursion/lab1-recursion.py
+++ Recursion/lab1-recursion.py
@@ -2,11 +2,20 @@
 	Lab1 - Recursion
 """
 
-def fac(n):
+def fac(n): # return n!
 	if n == 1:
 		return 1
 	else:
 		return fac(n-1) * n
 
+def sum1ToN(n): # return sum of 1 -> n while n >= 1
+	if n == 1:
+		return n
+	else:
+		if n < 1 :
+			return 'Error'
+		else:
+			return sum1ToN(n-1)+n
+
 if __name__ == '__main__':
-	print(fac(7))
+	print(sum1ToN(7))
